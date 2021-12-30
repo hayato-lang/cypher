59539b88a86afc97e909625904b6957149f24014
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-30 09:14:50 +0900

    ページの作成

---
+++ src/components/pages/ShowEvent.tsx
@@ -0,0 +1,7 @@
+import { memo, VFC } from "react";
+
+export const ShowEvent: VFC = memo(() => {
+  return (
+    <p>イベントの詳細ページ</p>
+  );
+});
---
+++ src/components/pages/ShowUser.tsx
@@ -0,0 +1,7 @@
+import { memo, VFC } from "react";
+
+export const ShowUser: VFC = memo(() => {
+  return (
+    <p>ユーザーの詳細ページ</p>
+  );
+});
