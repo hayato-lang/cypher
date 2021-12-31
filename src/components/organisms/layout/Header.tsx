ab57cee4926a4d7eac5697d921e8881fb30aa61c
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-31 14:34:33 +0900

    headerの作成

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,8 +1,18 @@
-import { Box, Flex, Heading, Link } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Flex, Heading, IconButton, Link, DrawerOverlay, DrawerBody,
+  DrawerContent, Button, Drawer, useDisclosure } from "@chakra-ui/react";
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router-dom";
 
 export const Header: VFC = memo(() => {
+  const history = useHistory();
+  const {isOpen, onOpen, onClose} = useDisclosure();
+
+  const onClickHome = useCallback(() => history.push("/home"), [history]);
+  const onClickShowUser = useCallback(() => history.push("/home/show_user"), [history]);
+  const onClickShowEvent = useCallback(() => history.push("/home/show_event"), [history]);
   return (
+  <>
     <Flex 
       as="nav"
       bg="gray.300"
@@ -10,7 +20,7 @@ export const Header: VFC = memo(() => {
       align="center"
       justify="space-between"
       padding={{ base: 2, md: 4 }}
-      opacity={0.8}
+      opacity={0.6}
     >
       <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
         <Heading 
@@ -18,6 +28,7 @@ export const Header: VFC = memo(() => {
           fontSize='2xl'
           fontFamily="Kaushan Script"
           color="blue.300"
+          onClick={onClickHome}
           >
           Cypher
         </Heading >
@@ -29,10 +40,30 @@ export const Header: VFC = memo(() => {
         display={{ base: "none", md: "flex" }}
       >
         <Box pr={4}>
-          <Link fontFamily="Kaushan Script">Users</Link>
+          <Link fontFamily="Kaushan Script" color="yellow.200" onClick={onClickShowUser}>Users</Link>
         </Box>
-        <Link fontFamily="Kaushan Script">Events</Link>
+        <Link fontFamily="Kaushan Script" color="yellow.200" onClick={onClickShowEvent}>Events</Link>
       </Flex>
+      <IconButton 
+        aria-label="メニューボタン" 
+        icon={<HamburgerIcon />}
+        size="sm"
+        variant="unstyled"
+        display={{ base: "block", md: "none" }}
+        onClick={onOpen}
+      />
     </Flex>
+    <Drawer placement="left" size="xs" isOpen={isOpen} onClose={onClose} >
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerBody bg="blue.300" p={0}>
+            <Button w="100%">HOME</Button>
+            <Button w="100%">ユーザー一覧</Button>
+            <Button w="100%">イベント一覧</Button>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  </>
   );
 });
