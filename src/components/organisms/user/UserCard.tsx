6d6ec0a938d47aa6c1dd82e36521fdd0d9edd48a
hayato-lang <j.h.14.s.g.8@gmail.com>
2022-01-05 14:22:25 +0900

    ユーザー詳細の実装

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -2,14 +2,15 @@ import { Box, Image, Stack, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 type Props = {
+  id: number;
   username: string;
   imageUrl: string;
   name: string;
-  onClick: () => void;
+  onClick: (id: number) => void;
 }
 
 export const UserCard: VFC<Props> = memo((props) => {
-  const { imageUrl, username, name, onClick } = props;
+  const { id, imageUrl, username, name, onClick } = props;
   return (
         <Box
           w="260px"
@@ -18,7 +19,7 @@ export const UserCard: VFC<Props> = memo((props) => {
           borderRadius= "10px"
           shadow="md"
           p={4}
-          onClick={onClick}
+          onClick={() => onClick(id)}
         >
           <Stack textAlign="center">
             <Image 
--- src/components/pages/ShowUser.tsx
+++ src/components/pages/ShowUser.tsx
@@ -9,16 +9,20 @@ import {
 } from "@chakra-ui/react";
 import { memo, useCallback, useEffect, VFC } from "react";
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { useSelectUser } from "../../hooks/useSelectUser";
 import { UserCard } from "../organisms/user/UserCard";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 export const ShowUser: VFC = memo(() => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
 
   useEffect(() => getUsers(), []);
 
-  const onClickOpen = useCallback(() => onOpen(), []);
+  const onClickOpen = useCallback((id: number) => {
+    onSelectUser({ id, users, onOpen })
+  }, []);
   return (
     <>
     {loading ? (
@@ -30,6 +34,7 @@ export const ShowUser: VFC = memo(() => {
             {users.map((user) => (
               <WrapItem key={user.id}>
                  <UserCard 
+                 id={user.id}
                    imageUrl="https://source.unsplash.com/random"
                    username={user.username}
                    name={user.name}
