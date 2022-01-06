5a78a5c4fe0189f3747a258dea626c92f1c3178c
hayato-lang <j.h.14.s.g.8@gmail.com>
2022-01-06 17:19:29 +0900

    スライダーをインストール

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,29 @@
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { memo, useEffect, VFC } from "react";
+import { useAllEvents } from "../../hooks/useAllEvents";
+import Slider, { Settings } from "react-slick";
+
+import "slick-carousel/slick/slick.css";
 
 export const Home: VFC = memo(() => {
+  const setting: Settings = {
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 3,
+    autoplay: true,
+    speed: 500,
+    cssEase: "ease-out"
+  }
+  const { getEvents, loading, events } = useAllEvents();
+
+
+  useEffect(() => getEvents(), []);
+
   return (
-    <p>ホーム画面</p>
+    <>
+    
+    </>
   );
 });
