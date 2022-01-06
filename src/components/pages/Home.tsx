1dbbf4056a680aa422e6f41b31d2a05b49d15099
hayato-lang <j.h.14.s.g.8@gmail.com>
2022-01-07 07:26:57 +0900

    スライダーを利用

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,29 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import { memo, useEffect, VFC } from "react";
-import { useAllEvents } from "../../hooks/useAllEvents";
+import { memo } from "react";
 import Slider, { Settings } from "react-slick";
 
 import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
 
-export const Home: VFC = memo(() => {
-  const setting: Settings = {
-    dots: true,
-    infinite: true,
-    centerMode: true,
-    slidesToShow: 3,
-    autoplay: true,
-    speed: 500,
-    cssEase: "ease-out"
-  }
-  const { getEvents, loading, events } = useAllEvents();
+const setting: Settings = {
+  dots: true,
+  infinite: true,
+  centerMode: true,
+  slidesToShow: 1,
+  autoplay: true,
+  speed: 500,
+};
 
 
-  useEffect(() => getEvents(), []);
+export const Home: React.VFC = memo(() => {
 
   return (
-    <>
-    
-    </>
+    <div>
+      <h2>Pick Up</h2>
+      <Slider {...setting}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
   );
 });
