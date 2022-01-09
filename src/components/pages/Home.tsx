cfe7053e6744d0672b40ea2a2edec1809f942d3b
hayato-lang <j.h.14.s.g.8@gmail.com>
2022-01-10 08:51:00 +0900

    スライドUserを追加

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,16 +1,64 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import { memo } from "react";
+import { memo, useEffect } from "react";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"
 
 import { SliderEvents } from "../organisms/event/SliderEvents";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { Center, Spinner } from "@chakra-ui/react";
+import Slider, { Settings } from "react-slick";
+import { HomeUserCard } from "../organisms/user/HomeUserCard";
 
-
+const settings: Settings = { 
+  dots: true,
+  infinite: true,
+  centerMode: true,
+  speed: 500,
+  cssEase: "ease-out",
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    { breakpoint: 640,
+    settings: {
+      slidesToShow: 1,
+    },
+  },
+  ],
+};
 
 export const Home: React.VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers, []);
   return (
-    <SliderEvents />
+    <>
+    {loading ? (
+      <Center>
+        <Spinner/>
+      </Center>
+    ) : (
+      <>
+        <SliderEvents />
+        <Slider {...settings}>
+          {users.map((user) => (
+            <Center>
+              <HomeUserCard 
+              key={user.id}
+              username={user.username}
+              imageUrl="https://source.unsplash.com/random"
+              name={user.name}
+              />
+            </Center>
+          ))}
+        </Slider>
+      </>
+    )}
+    </>
   );
 });
